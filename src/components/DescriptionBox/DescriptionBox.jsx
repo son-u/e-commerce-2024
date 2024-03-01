c19f958ec027c7b0a721f199fa7421bd13e9f6ad
Sonu <sonumrx37@gmail.com>
2024-03-01 10:15:11 +0530

    Completed Signup Pages

---
+++ src/components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "./DescriptionBox.css";
+
+const DescriptionBox = () => {
+  return (
+    <div className="descriptionbox">
+      <div className="descriptionbox-navigator">
+        <div className="descriptionbox-nav-box">Description</div>
+        <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+      </div>
+      <div className="descriptionbox-description">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis,
+          autem nihil repellat, soluta, nulla ipsam consequuntur ea enim quo
+          exercitationem repellendus aperiam distinctio. Quam ducimus suscipit
+          cumque eveniet, delectus tempore explicabo debitis alias quasi error,
+          voluptatum soluta dolores, numquam laudantium officiis consequuntur
+          accusamus beatae pariatur inventore voluptas vel quod. Et?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt quam
+          nam sunt corrupti tempora, eligendi, eius velit tempore, repellendus
+          labore omnis. Ducimus minus dolores maiores magni praesentium minima
+          quia quis?
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default DescriptionBox;
---
+++ src/context/ShopContext.jsx
@@ -0,0 +1,35 @@
+import React, { createContext, useState } from "react";
+import all_product from "../components/Assets/all_product";
+
+export const ShopContext = createContext(null);
+
+const getDefaultCart = () => {
+  let cart = {};
+  for (let index = 0; index < all_product.length+1; index++) {
+    cart[index] = 0;
+  }
+  return cart;
+};
+
+const ShopContextProvider = (props) => {
+  const [cartItems, setCartItems] = useState(getDefaultCart);
+  const contextValue = { all_product, cartItems };
+
+
+
+  const addToCart = () => {
+    
+  }
+
+
+
+
+
+  return (
+    <ShopContext.Provider value={contextValue}>
+      {props.children}
+    </ShopContext.Provider>
+  );
+};
+
+export default ShopContextProvider;
